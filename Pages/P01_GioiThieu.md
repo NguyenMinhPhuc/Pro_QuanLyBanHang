96d7744b3d8bc9a50f00ca3b1e4d3455e583e350
phuc <phuc@lhu.edu.vn>
2024-02-26 14:15:33 +0700

    init

---
+++ Pages/P01_GioiThieu.md
@@ -0,0 +1,14 @@
+# Mục lục
+- [Mục tiêu](#mục_tiêu)
+
+
+
+
+
+
+[Top](#mục_Tiêu)
+
+# Mục tiêu
+# Yêu cầu của dự án
+# Mô hình dữ liệu
+# Tham khảo
---
+++ QuanLyBanHang/QuanLyHanHang.WFA/HeThong/Frm_Connection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyHanHang.WFA.HeThong
+{
+    public partial class Frm_Connection : Form
+    {
+        public Frm_Connection()
+        {
+            InitializeComponent();
+        }
+    }
+}
